3eae916120bc6fb3141d64165e14c2aaf4ea846e
yuanyan <yuanyan.cao@gmail.com>
2014-04-04 00:21:43 +0800

    Page for display submitted packages.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$.ajax({
+    url: '/packages.json',
+    dataType: 'json',
+    success: function( data ) {
+        var packages = [];
+        data.forEach(function(pkg){
+            packages.push('<li><a target="_blank" href="http://npmjs.org/' + pkg.name + '"><name>' + pkg.name+ '</name><desc>' + pkg.description + '</desc></a></li>');
+        })
+        $('#packages').html(packages.join(''))
+    }
+});
